145fb4a5473823d5ab2035f62dafb1b7af22476d
Tux <clark@dev-engine.net>
2018-02-21 22:05:13 +0800

    side menu for mobile phones

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
 (function() {
     var css = {};
     var c = false;
+    var open = false;
+    var closeIt = function() {
+        $('.drawer').css({'left' : '0px'});
+        $('.side-menu').css({'left' : '-130px'});
+        $('.profile-detail').css({'left' : '-120px'});
+    }
+    
     $('.profile-detail, .about').click(function(e) {
         e.preventDefault();
+        if(c) {
+            return;
+        }
+        
         c = true;
         var offset = $('.profile-detail').offset();
         css = {
@@ -10,14 +21,18 @@
             'top' : offset.top,
             'left' : offset.left
           };
+
+        if ($(window).width() < 750) {
+            $('.drawer').show();
+        }
         
         $('.profile-detail').css(css);
         $('.footer').hide();
-        $('.main-content').css({'margin-top' : '20px'});
+        $('.main-content').css({'margin-top' : '50px'});
         $('h1, .slogan, .all-right, hr').hide();
         setTimeout(function() {
             var mCss = $('.main-content').offset();
-            $('.profile-detail').css({'top' : '30px', 'left' : mCss.left-130, 'z-index' : -10, 'width' : '112px'});
+            $('.profile-detail').css({'top' : '30px', 'left' : mCss.left-130, 'width' : '112px'});
             $('.profile-img').css({'width' : '100px', 'height': '100px', 'max-width' : '100px'})
             $('.profile-img img').css({'margin-left' : '-20px'});
             $('h1').css({'text-align' : 'left'});
@@ -26,7 +41,13 @@
             $('.me').fadeIn(500);
             setTimeout(function() {
                 var nOffset = $('.profile-detail').offset();
-                $('.side-menu').css({'left' : mCss.left-130, 'top' : nOffset.top+130});
+                var sTop = nOffset.top+130;
+                if ($(window).width() < 750) {
+                    sTop = 0;
+                    $('.drawer').show();
+                }
+                
+                $('.side-menu').css({'left' : mCss.left-130, 'top' : sTop});
                 $('.side-menu').fadeIn(500);
             }, 300);
         }, 0);
@@ -35,6 +56,8 @@
     $('.home-back').click(function(e) {
         e.preventDefault();
         c = false;
+        closeIt();
+        $('.drawer').hide();
         $('.profile-detail').css({top: css.top, left: css.left, width: 312, 'max-width': 312});
         $('.profile-img').css({'width' : '300px', 'height': '300px', 'max-width' : '300px'})
         $('.profile-img img').css({'margin-left' : '-50px'});
@@ -56,10 +79,25 @@
             $('h1').removeAttr('style');
             $('.slogan').removeAttr('style');
             $('.side-menu').removeAttr('style');
+            $('.side-menu').css({'left' : '0px', 'padding-top' : '150px', 'border' : '1px solid #eee'});
+            $('')
             $('.footer').removeAttr('style');
         },500);
     })
 
+    $('.drawer').click(function() {
+        if (open) {
+            open = false;
+            closeIt();
+            return;
+        }
+
+        open = true;
+        $('.profile-detail').css({'left' : '10px'});
+        $('.side-menu').css({'left' : '0px'});
+        $(this).css({'left' : '130px'});
+    });
+    
     $(window).resize(function() {
         if (!c) {
             return;
@@ -67,7 +105,14 @@
         
         var mCss = $('.main-content').offset();
         var nOffset = $('.profile-detail').offset();
-        $('.profile-detail').css({'top' : '30px', 'left' : mCss.left-130, 'z-index' : -10, 'width' : '112px'});
-        $('.side-menu').css({'left' : mCss.left-130, 'top' : nOffset.top+130});
+        $('.profile-detail').css({'top' : '30px', 'left' : mCss.left-130, 'width' : '112px'});
+        $('.side-menu').css({'left' : mCss.left-130, 'top' : nOffset.top+130, 'border' : 'none', 'padding-top' : 0});
     });
+
+    if ($(window).width() < 750) {
+        $('.profile-detail').css({'left' : '10px'});
+        $('.side-menu').css('cssText', 'top 0px !important');
+        $('.side-menu').css({'left' : '0px', 'padding-top' : '150px', 'border' : '1px solid #eee'});
+    }
+    
 }(jQuery));
